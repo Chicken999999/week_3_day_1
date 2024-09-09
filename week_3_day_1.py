2bdc1d37811279f11c721ec292e4e474eacde9c5
Alfaro <caalfaro1@cps.k12.il.us>
2024-09-09 12:43:12 -0500

    variables

--- week_3_day_1.py
+++ week_3_day_1.py
@@ -50,22 +50,27 @@
 # # Review
 # create variables for the following :
 # 1. age
+age = 17
 # 2. name
+name = "Cristofer"
 # 3. song
+song = "Mewtwo"
 # 4. food
+food = "pizza"
 # 5. number
-
+number = 9
 
 # #now include the variables you just made print in the following...
 
 
 # Once upon a time, there was a [age] old coder named [name].
-
+print(f"Once upon a time, there was a {age} old coder named {name}.")
 
 # [name] liked to hum the song [song] while coding. It was so annoying that their teammates would throw [food] until [name] would stop singing.
-
+print(f"{name} liked to hum the song {song} while coding. It was so annoying that their teammates would throw {food} until {name} would stop singing." )
 
 # Still, [name] was the best coder on the team and could write [number] lines of code every day. Maybe [song] was [name]’s secret power?
+print(f"Still, {name} was the best coder on the team and could write {number} lines of code every day. Maybe {song} was {name}'s secret power?")
 ##########################################################################################
 
 
